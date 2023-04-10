47c22999789acb948183a0a22fd2101fb9650a60
Jasmin Maier <jatrice.maier@gmail.com>
2023-04-10 18:37:36 +0200

    RPS Bonus Features

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -1,14 +1,32 @@
 const readline = require('readline-sync');
-const VALID_CHOICES = ['rock', 'paper', 'scissors'];
+const VALID_CHOICES = ['rock', 'paper', 'scissors', 'spock', 'lizard'];
+let userScore = 0;
+let computerScore = 0;
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-function sanitizeInput(input) {
-  return input.replaceAll(' ', '').replace('.', '').toLowerCase();
+function parseShortCut(input) {
+  switch (input) {
+    case 'r':
+      return 'rock';
+    case 'p':
+      return 'paper';
+    case 'sc':
+      return 'scissors';
+    case 'l':
+      return 'lizard';
+    case 'sp':
+      return 'spock';
+    default:
+      return input;
+  }
 }
 
+function sanitizeInput(input) {
+  return parseShortCut(input.replaceAll(' ', '').replace('.', '').toLowerCase());
+}
 function isInvalidInput(input) {
   return !VALID_CHOICES.includes(input) ||
   input.trim() === '';
@@ -27,17 +45,22 @@ function getUserChoice(message) {
 }
 
 function displayWinner (choice, computerChoice) {
-  if ((choice === 'rock' && computerChoice === 'scissors') ||
-    (choice === 'scissors' && computerChoice === 'paper') ||
-    (choice === 'paper' && computerChoice === 'rock')) {
-    prompt('You win!');
-  } else if ((choice === 'scissors' && computerChoice === 'rock') ||
-    (choice === 'paper' && computerChoice === 'scissors') ||
-    (choice === 'rock' && computerChoice === 'paper')) {
-    prompt('The computer wins this round!');
-  } else {
+  if (choice === computerChoice) {
     prompt(`Since you both picked ${choice}, it's a tie!`);
+    return;
   }
+  if ((choice === 'rock' && (computerChoice === 'lizard' || computerChoice === 'scissors')) ||
+    (choice === 'paper' && (computerChoice === 'rock' || computerChoice === 'spock')) ||
+    (choice === 'scissors' && (computerChoice === 'paper' || computerChoice === 'lizard')) ||
+    (choice === 'lizard' && (computerChoice === 'spock' || computerChoice === 'paper')) ||
+    (choice === 'spock' && (computerChoice === 'scissors' || computerChoice === 'rock'))) {
+    prompt('You win!');
+    userScore += 1;
+    return;
+  }
+
+  prompt('The computer wins this round!');
+  computerScore += 1;
 }
 
 function isValidYesNoResponse(answer) {
@@ -57,21 +80,53 @@ function playAgain() {
   return (answer === 'y' || answer === 'yes');
 }
 
-//Start of the interaction
-
-console.clear();
-prompt("Let's play a game!");
-
-do {
+function oneRound() {
   let choice = getUserChoice(`Choose one: ${VALID_CHOICES.join(', ')}`);
 
   let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
   let computerChoice = VALID_CHOICES[randomIndex];
 
-  prompt(`Alright let's see who won.
-   You chose ${choice}. The computer chose ${computerChoice}.`);
+  prompt(`You chose ${choice}. The computer chose ${computerChoice}.`);
 
   displayWinner(choice, computerChoice);
+}
+
+function playGame() {
+  let gameCount = 0;
+  do {
+    oneRound();
+    prompt(`The current score is: ${userScore} for you, ${computerScore} for the computer.`);
+    // prompt('Hit a key to continue.');
+    // readline.question();
+    // console.clear();
+    gameCount += 1;
+  } while (gameCount <= 5 && (userScore < 3 && computerScore < 3));
+}
+
+function finalWinner() {
+  if (userScore > computerScore) {
+    prompt('Congratulations! You win the game!');
+    return;
+  }
+  if (computerScore > userScore) {
+    prompt('Oh no, the computer beat you this time!');
+    return;
+  }
+
+  prompt("It's a tie overall!");
+}
+
+//Start of the interaction
+
+console.clear();
+prompt("Hey there, let's play a game!\n   " +
+  "The rules are simple, whoever wins the most out of 5 rounds, wins the game!");
+
+do {
+  userScore = 0;
+  computerScore = 0;
+  playGame();
+  finalWinner();
 } while (playAgain());
 
 console.clear();
