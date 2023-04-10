bd7115eb7f1c8cf0caf42cb3ea7e56a7b3b5325a
Jasmin Maier <jatrice.maier@gmail.com>
2023-04-10 19:37:54 +0200

    RPS Bonus Features Edit

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -1,7 +1,5 @@
-const readline = require('readline-sync');
-const VALID_CHOICES = ['rock', 'paper', 'scissors', 'spock', 'lizard'];
-let userScore = 0;
-let computerScore = 0;
+const readline = require("readline-sync");
+const VALID_CHOICES = ["rock", "paper", "scissors", "spock", "lizard"];
 
 function prompt(message) {
   console.log(`=> ${message}`);
@@ -9,27 +7,26 @@ function prompt(message) {
 
 function parseShortCut(input) {
   switch (input) {
-    case 'r':
-      return 'rock';
-    case 'p':
-      return 'paper';
-    case 'sc':
-      return 'scissors';
-    case 'l':
-      return 'lizard';
-    case 'sp':
-      return 'spock';
+    case "r":
+      return "rock";
+    case "p":
+      return "paper";
+    case "sc":
+      return "scissors";
+    case "l":
+      return "lizard";
+    case "sp":
+      return "spock";
     default:
       return input;
   }
 }
 
 function sanitizeInput(input) {
-  return parseShortCut(input.replaceAll(' ', '').replace('.', '').toLowerCase());
+  return parseShortCut(input.replaceAll(" ", "").replace(".", "").toLowerCase());
 }
 function isInvalidInput(input) {
-  return !VALID_CHOICES.includes(input) ||
-  input.trim() === '';
+  return !VALID_CHOICES.includes(input) || input.trim() === "";
 }
 
 function getUserChoice(message) {
@@ -44,72 +41,83 @@ function getUserChoice(message) {
   return choice;
 }
 
-function displayWinner (choice, computerChoice) {
+function didUserWin(choice, computerChoice) {
+  return ((choice === "rock" && (computerChoice === "lizard" || computerChoice === "scissors")) ||
+    (choice === "paper" && (computerChoice === "rock" || computerChoice === "spock")) ||
+    (choice === "scissors" && (computerChoice === "paper" || computerChoice === "lizard")) ||
+    (choice === "lizard" && (computerChoice === "spock" || computerChoice === "paper")) ||
+    (choice === "spock" && (computerChoice === "scissors" || computerChoice === "rock")));
+}
+
+function displayWinner(choice, computerChoice, scores) {
   if (choice === computerChoice) {
     prompt(`Since you both picked ${choice}, it's a tie!`);
     return;
   }
-  if ((choice === 'rock' && (computerChoice === 'lizard' || computerChoice === 'scissors')) ||
-    (choice === 'paper' && (computerChoice === 'rock' || computerChoice === 'spock')) ||
-    (choice === 'scissors' && (computerChoice === 'paper' || computerChoice === 'lizard')) ||
-    (choice === 'lizard' && (computerChoice === 'spock' || computerChoice === 'paper')) ||
-    (choice === 'spock' && (computerChoice === 'scissors' || computerChoice === 'rock'))) {
-    prompt('You win!');
-    userScore += 1;
+  if (didUserWin(choice, computerChoice)) {
+    prompt("You win!");
+    scores.userScore += 1;
+
     return;
   }
 
-  prompt('The computer wins this round!');
-  computerScore += 1;
+  prompt("The computer wins this round!");
+  scores.computerScore += 1;
 }
 
 function isValidYesNoResponse(answer) {
-  return (answer === 'y' || answer === 'yes' ||  answer === 'n' || answer === 'no');
+  return answer === "y" || answer === "yes" || answer === "n" || answer === "no";
 }
 
 function playAgain() {
-  prompt('Would you like to play again?');
+  prompt("Would you like to play again?");
   let answer = sanitizeInput(readline.question());
   console.clear();
 
   while (!isValidYesNoResponse(answer)) {
-    prompt('Please type in a valid yes/no answer.');
+    prompt("Please type in a valid yes/no answer.");
     answer = sanitizeInput(readline.question());
   }
 
-  return (answer === 'y' || answer === 'yes');
+  return answer === "y" || answer === "yes";
 }
 
-function oneRound() {
-  let choice = getUserChoice(`Choose one: ${VALID_CHOICES.join(', ')}`);
+function oneRound(scores) {
+  let choice = getUserChoice(`Choose one: ${VALID_CHOICES.join(", ")}`);
 
   let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
   let computerChoice = VALID_CHOICES[randomIndex];
 
   prompt(`You chose ${choice}. The computer chose ${computerChoice}.`);
 
-  displayWinner(choice, computerChoice);
+  displayWinner(choice, computerChoice, scores);
 }
 
-function playGame() {
+function promptToContinue(gameCount, scores) {
+  if (gameCount <= 5 && scores.userScore < 3 && scores.computerScore < 3) {
+    prompt("Hit a key to continue.");
+    readline.question();
+    console.clear();
+  }
+}
+
+function playGame(scores) {
   let gameCount = 0;
   do {
-    oneRound();
-    prompt(`The current score is: ${userScore} for you, ${computerScore} for the computer.`);
-    // prompt('Hit a key to continue.');
-    // readline.question();
-    // console.clear();
+    oneRound(scores);
+    prompt(`The current score is: ${scores.userScore} for you, ${scores.computerScore} for the computer.`);
     gameCount += 1;
-  } while (gameCount <= 5 && (userScore < 3 && computerScore < 3));
+    promptToContinue(gameCount, scores);
+  } while (gameCount <= 5 && scores.userScore < 3 && scores.computerScore < 3);
 }
 
-function finalWinner() {
-  if (userScore > computerScore) {
-    prompt('Congratulations! You win the game!');
+function finalWinner(scores) {
+  if (scores.userScore > scores.computerScore) {
+    prompt("Congratulations! You win the game!");
     return;
   }
-  if (computerScore > userScore) {
-    prompt('Oh no, the computer beat you this time!');
+  if (scores.computerScore > scores.userScore) {
+    prompt("Oh no, the computer beat you this time!");
     return;
   }
 
@@ -119,15 +127,15 @@ function finalWinner() {
 //Start of the interaction
 
 console.clear();
-prompt("Hey there, let's play a game!\n   " +
-  "The rules are simple, whoever wins the most out of 5 rounds, wins the game!");
+prompt(
+  "Hey there, let's play a game!\n   " +
+  "The rules are simple, whoever wins the most out of 5 rounds, wins the game!"
+);
 
 do {
-  userScore = 0;
-  computerScore = 0;
-  playGame();
-  finalWinner();
+  const scores = { userScore: 0, computerScore: 0 };
+  playGame(scores);
+  finalWinner(scores);
 } while (playAgain());
 
 console.clear();
-
